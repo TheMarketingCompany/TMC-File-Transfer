f408b0ad2e2bce0b18b1a463e95ec920543a57c3
schulzjona <jonajonajona73@gmail.com>
2022-06-24 16:01:08 +0200

    Update [filehash].js

--- functions/api/transfer/get/[filehash].js
+++ functions/api/transfer/get/[filehash].js
@@ -1,4 +1,4 @@
 export async function onRequestGet({request, env, params}) {
     const data = await env.transfer.get(params.filehash)
-    return new Response(JSON.parse(data));
+    return new Response(data);
 }
