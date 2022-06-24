91f20fed7448719434ce6615bb30f128f22b15d5
schulzjona <jonajonajona73@gmail.com>
2022-06-24 16:17:01 +0200

    Update [filehash].js

--- functions/api/transfer/get/[filehash].js
+++ functions/api/transfer/get/[filehash].js
@@ -1,5 +1,5 @@
 export async function onRequestGet({request, env, params}) {
     const data = await env.transfer.get(params.filehash)
-    delete data.options.passwordHash
+    
     return new Response(data);
 }
