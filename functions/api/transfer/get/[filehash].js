0675617d21fb3dc1af58393c663f778148731c2d
schulzjona <jonajonajona73@gmail.com>
2022-06-24 16:26:29 +0200

    Update [filehash].js

--- functions/api/transfer/get/[filehash].js
+++ functions/api/transfer/get/[filehash].js
@@ -1,12 +1,5 @@
 export async function onRequestGet({request, env, params}) {
-    const data = JSON.parse(await env.transfer.get(params.filehash))
-
-    return new Response(JSON.stringify({
-        filename: data.filename,
-        options: {
-            lifetime: data.options.lifetime,
-            otd: data.options.otd,
-            passwordEnabled: data.options.passwordEnabled
-        }
-    }));
+    const data = await env.transfer.get(params.filehash)
+    
+    return new Response(data);
 }
