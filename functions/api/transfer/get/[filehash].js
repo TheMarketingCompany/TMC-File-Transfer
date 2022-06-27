817e79d606657248f89ed7f75ece2df718f23a0d
schulzjona <jonajonajona73@gmail.com>
2022-06-27 15:07:02 +0200

    Update [filehash].js

--- functions/api/transfer/get/[filehash].js
+++ functions/api/transfer/get/[filehash].js
@@ -4,6 +4,8 @@ export async function onRequestGet({request, env, params}) {
         data = JSON.parse(await env.transfer.get(params.filehash))
         if (!data) return new Response(null, {status: 404, statusText: 'file not found'})
 
+        if (data.options.otd === true && data.downloadCount > 0) return new Response(null, {status: 404, statusText: 'file not found'})
+
         return new Response(JSON.stringify(data));
     } catch (e) {
         return new Response(e, {status: 501})
