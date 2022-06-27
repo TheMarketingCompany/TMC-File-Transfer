4f8b3a38d63942c3289cc1027e74f13e343bcab3
schulzjona <jonajonajona73@gmail.com>
2022-06-27 11:21:42 +0200

    Update [filename].js

--- functions/api/transfer/create/[filename].js
+++ functions/api/transfer/create/[filename].js
@@ -7,9 +7,18 @@ export async function onRequestPost({request, params, env}) {
     let timeout
 
     switch(options.lifetime) {
+        case "1 day":
+            timeout = (Math.round(+new Date() / 1000)) + 86400 // 86400 => 1 day
+            break;
         case "1 week":
             timeout = (Math.round(+new Date() / 1000)) + 604800 // 604800 => 1 week
-
+            break;
+        case "1 month":
+            timeout = (Math.round(+new Date() / 1000)) + 2592000 // 2592000 => 1 month
+            break;
+        default:
+            timeout = (Math.round(+new Date() / 1000)) + 604800 // 604800 => 1 week
+            break;
     }
 
     await env.transfer.put(uuid, JSON.stringify({
