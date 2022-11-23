12d3f4404b69b013e75ba0bf1a0453ea9db94696
schulzjona <jonajonajona73@gmail.com>
2022-11-23 11:04:35 +0100

    Update [filehash].js

--- functions/api/transfer/get/[filehash].js
+++ functions/api/transfer/get/[filehash].js
@@ -1,14 +1,38 @@
-export async function onRequestGet({request, env, params}) {
-    let data
+export async function onRequestGet({ request, env, params }) {
+    let data;
     try {
-        data = JSON.parse(await env.transfer.get(params.filehash))
-        if (!data) return new Response(null, {status: 404, statusText: 'file not found'})
+        data = JSON.parse(await env.transfer.get(params.filehash));
+        if (!data) return new Response(null, { status: 404, statusText: 'file not found' });
 
-        if (data.options.otd === true && data.downloadCount > 0) return new Response(null, {status: 404, statusText: 'file not found'})
+        if (data.options.otd === true && data.downloadCount > 0) return new Response(null, {
+            status: 404,
+            statusText: 'file not found',
+        });
+
+        if (expired(data.timeout * 1000) === true) return new Response(null, {
+            status: 404,
+            statusText: 'file not found',
+        });
 
         return new Response(JSON.stringify(data));
     } catch (e) {
-        return new Response(e, {status: 501})
+        return new Response(e, { status: 501 });
     }
 
 }
+
+function expired(endTime) {
+    const endDate = new Date(endTime);
+
+    const now = new Date();
+
+    const distance = endDate - now;
+
+    if (distance < 0) {
+        return 'true';
+    } else {
+        return false;
+    }
+}
+
+,
