a8a4a475bb1585a26756482b0a267e7b43bfe718
Jona <jonajonajona73@gmail.com>
2024-04-23 11:54:48 +0200

    testing downloadfilename

--- functions/api/transfer/validate/[filehash].js
+++ functions/api/transfer/validate/[filehash].js
@@ -13,6 +13,8 @@ async function generatePresignedUrl(objectKey, endpoint, bucket) {
     const signed = await r2.sign(new Request(url, {
         method: 'GET'
     }), {
+        'Content-Disposition': 'attachment; filename=' + objectKey,
+    }, {
         aws: {
             signQuery: true
         }
